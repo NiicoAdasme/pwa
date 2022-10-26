3306c933a76d0542d3cc51ef224508fa65af497d
NiicoAdasme <nicolas.wladimir@gmail.com>
2022-10-26 14:53:59 -0300

    first commit

---
+++ pwa/src/App.js
@@ -0,0 +1,60 @@
+import { useState } from 'react';
+
+import logo from './logo.svg';
+import './App.css';
+import { useEffect } from 'react';
+
+
+function App() {
+
+  const [isReadyForInstall, setIsReadyForInstall] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("beforeinstallprompt", (event) => {
+      // Prevent the mini-infobar from appearing on mobile.
+      event.preventDefault();
+      console.log("👍", "beforeinstallprompt", event);
+      // Stash the event so it can be triggered later.
+      window.deferredPrompt = event;
+      // Remove the 'hidden' class from the install button container.
+      setIsReadyForInstall(true);
+    });
+  }, []);
+
+  async function downloadApp() {
+    console.log("👍", "butInstall-clicked");
+    const promptEvent = window.deferredPrompt;
+    if (!promptEvent) {
+      // The deferred prompt isn't available.
+      console.log("oops, no prompt event guardado en window");
+      return;
+    }
+    // Show the install prompt.
+    promptEvent.prompt();
+    // Log the result
+    const result = await promptEvent.userChoice;
+    console.log("👍", "userChoice", result);
+    // Reset the deferred prompt variable, since
+    // prompt() can only be called once.
+    window.deferredPrompt = null;
+    // Hide the install button.
+    setIsReadyForInstall(false);
+  }
+
+  return (
+    <div className="App">
+      <header>
+        <h1>Progresive Web Aplication</h1>
+        {isReadyForInstall && (
+          <button onClick={downloadApp}> Descargasdasda </button>
+        )}
+        <img src={logo} className="App-logo" alt="logo" width={200} height={200} />
+      </header>
+
+
+
+    </div>
+  );
+}
+
+export default App;
